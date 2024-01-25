5b932e0d60731d7e1bccc94ce3170ffac2315dce
mdk-1 <59763792+mdk-1@users.noreply.github.com>
2024-01-25 16:13:58 +1100

    update

--- main.py
+++ main.py
@@ -38,8 +38,9 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
     if text == "/start":
         with open('hello.gif', 'rb') as photo:
             await bot.send_photo(chat_id=chat_id, photo=photo)
-        await bot.send_message(chat_id=chat_id, text="Welcome to Cyclic Starter Python Telegram Bot!")
+        await bot.send_message(chat_id=chat_id, text="Hello! I am Sanic AI and I will be able to assist you with any queries you have about the Sanicverse.")
     else:
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Yo!")
+    if text == "/info":
+        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Click the blue Sanicverse button in the lower left corner to get started!")
 
     return {"ok": True}
